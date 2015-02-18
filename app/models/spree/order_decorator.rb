ceff337920f7e4b7fbef01251eba75875145fdd5
Jason Hazel <jhazel@switchboxinc.com>
2015-02-18 14:33:02 -0500

    init.

---
+++ app/models/spree/order_decorator.rb
@@ -0,0 +1,13 @@
+Spree::Order.class_eval do 
+
+  after_save :update_order_number
+
+  def generate_order_number(options = {})
+    self.number = self.id || nil
+  end
+
+  def update_order_number
+    self.number ||= self.id
+  end
+
+end
---
+++ lib/spree_sequential_order_number.rb
@@ -0,0 +1,2 @@
+require 'spree_core'
+require 'spree_sequential_order_number/engine'
