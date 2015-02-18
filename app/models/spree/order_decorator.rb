44449112ae858a1140938f7630a3fb65402be38f
Jason Hazel <jhazel@switchboxinc.com>
2015-02-18 15:36:25 -0500

    after_save action to set number to model id

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,14 +1,17 @@
 Spree::Order.class_eval do 
 
-  after_save :update_order_number
+  after_save :update_order_number, if: :needs_order_number?
 
   def generate_order_number(options = {})
-    self.number = self.id || nil
+    '-1'
+  end
+
+  def needs_order_number?
+    self.number == '-1'
   end
 
   def update_order_number
-    self.number ||= self.id
-    save
+    update_attributes(number: self.id)
   end
 
 end
